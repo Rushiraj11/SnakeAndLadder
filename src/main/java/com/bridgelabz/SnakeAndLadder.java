1d083f97b84321cbc92abd0d776245863d338d65
Rushiraj11 <grushiraj95@gmail.com>
2021-08-27 15:11:56 +0530

    Player Rolling Dice

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -2,7 +2,9 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
+        // initialize player start position
         int position = 0;
-
+    // store value of Dice Roll
+        int diceNumber=(int) (Math.floor(Math.random() *10) % 6 + 1);
     }
 }
