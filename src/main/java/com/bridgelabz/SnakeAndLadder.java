254fa0ceae91e516f95dc6e21809b2abb17f99eb
Rushiraj11 <grushiraj95@gmail.com>
2021-08-27 17:30:58 +0530

    Added : Player win till reaches to Position 100

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -15,17 +15,26 @@ public class SnakeAndLadder {
             int option = (int) (Math.floor(Math.random() * 10) % 3 + 1);
             if (option == IS_SNAKE) { // if randomly player reaches position having snake ,it will enter loop
                 System.out.println("Snake for the player");
-                position = position - diceNumber; // Players position gets subtracted respectively
-                if (position < 0) {
-                    position = 0;
+               // position = position - diceNumber; // Players position gets subtracted respectively
+               // if (position < 0) {
+                //   position = 0;
+                //}
+                if (position - diceNumber > 0){
+                    position = position - diceNumber;
                 }
+
             } else if
             (option == IS_LADDER) { // if player does not get snake it will check for ladder randomly & enter loop
                 System.out.println("Ladder for the player");
                 position = position + diceNumber; // Players position gets added respectively
+             if ( position > 100){
+                 position = position - diceNumber;
+             }
+
             } else {
                 System.out.println(" No Play for the player"); // Will Remain to same position
             }
+            System.out.println(" dice: " + diceNumber );
             System.out.println("position" + position);
         }
     }
