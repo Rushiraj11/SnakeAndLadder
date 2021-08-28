70a410ece599029f2dba6448eaf31b08817f39ba
Rushiraj11 <grushiraj95@gmail.com>
2021-08-28 17:26:34 +0530

    Added: Two Players to the Game

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -4,15 +4,45 @@ public class SnakeAndLadder {
     public static final int IS_LADDER = 1; // Assigning Constant variables
     public static final int IS_SNAKE = 2;  // Assigning Constant variable
     public static final int WINNING_POSITION = 100;
-
+           static int diceCount=0;
+            static String turn ="player1";
     public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder");
         // initialize player start position
-        int position = 0;
-        int diceCount = 0; // Initialize dice count variable
-        while (position < WINNING_POSITION) {
-
+        int player1Position = 0;
+        int player2Position = 0;
+        int player1DiceCount = 0; // Initialize dice count variable for player 1
+        int player2DiceCount = 0; // Initialize dice count variable for player 2
+        while (player1Position < WINNING_POSITION && player2Position < WINNING_POSITION) {
             ++diceCount; // increment count
             // store value of Dice Roll
+          if (turn.equals("player1")) {
+              turn = "player2";
+              ++player1DiceCount;
+              player1Position = getPosition(player1Position);
+
+          }else {
+              turn = "player1";
+              ++player2DiceCount;
+              player2Position = getPosition(player2Position);
+
+          }
+          }
+           if (player1Position == WINNING_POSITION) {
+               System.out.println("Player1 win the game");
+               System.out.println("dice count " + player1DiceCount);
+           } else {
+               System.out.println("Player2 win the game");
+               System.out.println("dice count " + player2DiceCount);
+           }
+        System.out.println("Total dice count " + diceCount);
+
+        System.out.println("player1Position " + player1Position);
+        System.out.println("player2Position " + player2Position);
+
+    }
+
+            private static int getPosition(int position){
             int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
             // store the position value
             int option = (int) (Math.floor(Math.random() * 10) % 3 + 1);
@@ -29,10 +59,14 @@ public class SnakeAndLadder {
             } else if
             (option == IS_LADDER) { // if player does not get snake it will check for ladder randomly & enter loop
                 System.out.println("Ladder for the player");
-                position = position + diceNumber; // Players position gets added respectively
-             if ( position > 100){
-                 position = position - diceNumber;
-             }
+                if(turn.equals("player1")) {
+                    turn = "player1";
+                }else if (turn.equals("player2")) {
+                    turn = "player2";
+                }
+                 if(position +diceNumber <= 100) {
+                     position = position + diceNumber;
+                 }
 
             } else {
                 System.out.println(" No Play for the player"); // Will Remain to same position
@@ -40,7 +74,8 @@ public class SnakeAndLadder {
             System.out.println(" dice: " + diceNumber );
             System.out.println("Dice Count :" + diceCount);
             System.out.println("position" + position);
+            return position;
         }
     }
-}
+
 
